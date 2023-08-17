492b9d58a881ad7c5d99b8ef260c751154afe0fb
Faith Fechser <faithfechser@gmail.com>
2023-08-17 11:58:15 -0600

    connect database and code data add/collecting

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,21 +14,28 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  // connect database & make transaction
   const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  const request = store.put({ id: 1, value: content });
+  const transaction = jateDB.transaction("jate", "readwrite");
+  // add content code
+  const store = transaction.objectStore("jate");
+  const request = store.add({ jate: content });
+  // request confirmation
   const result = await request;
   console.log("Data Saved", result);
 };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  // connect database
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  // get all data from database
+  const request = store.getAll;
+  // request confirmation
   const result = await request;
-  console.log("Data Read", result);
+  console.log('result.value', result);
+  return result;
 };
 
 initdb();
