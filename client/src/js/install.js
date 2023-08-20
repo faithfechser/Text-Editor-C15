8642c1c87242e43b547832b6b1e75052f4d6a1de
Faith Fechser <faithfechser@gmail.com>
2023-08-20 03:17:26 -0600

    installation fix, code replacement

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,19 +1,53 @@
-const butInstall = document.getElementById('buttonInstall');
+// const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// // Logic for installing the PWA
+// // TODO: Add an event handler to the `beforeinstallprompt` event
+// window.addEventListener('beforeinstallprompt', (event) => {
+//     butInstall.style.visibility = 'visible';
+//     // TODO: Implement a click event handler on the `butInstall` element
+//     butInstall.addEventListener('click', async () => {
+//         event.prompt();
+//         butInstall.setAttribute('disabled', true);
+//         butInstall.textContent = 'Installed!';
+//     });
+// });
+
+// // TODO: Add an handler for the `appinstalled` event
+// window.addEventListener('appinstalled', (event) => {
+//     console.log('App Installation Success!', 'appinstalled', event);
+//     butInstall.textContent = 'Installation Success!';
+// });
+
+
+const butInstall = document.getElementById("buttonInstall");
+
 window.addEventListener('beforeinstallprompt', (event) => {
-    butInstall.style.visibility = 'visible';
-    // TODO: Implement a click event handler on the `butInstall` element
-    butInstall.addEventListener('click', async () => {
-        event.prompt();
-        butInstall.setAttribute('disabled', true);
-        butInstall.textContent = 'Installed!';
-    });
+
+    // Store the triggered events
+    window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
+  });
+
+butInstall.addEventListener('click', async () => {
+  
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+   return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
+  
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    console.log('App Installation Success!', 'appinstalled', event);
-    butInstall.textContent = 'Installation Success!';
-});
+  // Clear prompt
+  window.deferredPrompt = null;
+}); 
