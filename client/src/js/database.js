0071d4e32d132e635366de36e93ce4e55e7859b5
Faith Fechser <faithfechser@gmail.com>
2023-08-20 03:18:02 -0600

    debug site saving ability

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -35,7 +35,7 @@ export const getDb = async () => {
   // request confirmation
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
